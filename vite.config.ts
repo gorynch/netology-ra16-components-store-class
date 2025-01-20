9f844e284aba57eb964a483153ab1fd8ea7b24f6
gorynch <i231081@yandex.ru>
2025-01-20 19:00:14 +0300

    update some files cause of rename project

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ra16-homeworks-components-store-class/',
+  base: '/netology-ra16-components-store-class/',
   plugins: [react()],
 })
